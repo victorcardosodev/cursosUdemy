933d32b7f4c89be2394f84f237fdd0296f505adf
victorhenriq <victor_henriquesc@outlook.com>
2022-01-09 00:37:52 -0300

    Curso web completo

---
+++ EXERCICIOS-JS/fundamentos/comentarios.js
@@ -0,0 +1,18 @@
+//comentario de uma linha
+console.log("uma linha");
+
+
+/*
+ * comentario de
+ * multiplas linhas
+ * 
+ * de um modo mais bonitinho visualmente
+ * 
+ */
+
+
+
+/* comentario 
+de multiplas
+linhas tbm
+*/
