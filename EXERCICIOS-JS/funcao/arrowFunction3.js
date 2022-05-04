60aa49427c327413d9438b7f1b3ac87cd72a8368
victorhenriq <victor_henriquesc@outlook.com>
2022-05-03 23:57:29 -0300

    Curso web completo

---
+++ EXERCICIOS-JS/funcao/arrowFunction3.js
@@ -0,0 +1,22 @@
+const { Module } = require("module");
+
+let comparaComThis = function (param) {
+    console.log(this === param);
+}
+
+comparaComThis(global);
+
+const obj = { }
+comparaComThis = comparaComThis.bind(obj);
+
+comparaComThis(global);
+comparaComThis(obj);
+
+let comparaComThisArrow = param => console.log(this === param);
+comparaComThisArrow(global);
+comparaComThisArrow(obj);
+comparaComThisArrow(module.exports);
+
+comparaComThisArrow = comparaComThisArrow.bind(obj);
+comparaComThisArrow(obj);
+comparaComThisArrow(module.exports);
